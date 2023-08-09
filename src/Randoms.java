2fb0b334a28caf3d49fea348bd6a0aeb17446c60
VorobievKA <vorobievka@gmail.com>
2023-08-09 22:51:02 +0300

    New version

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,26 +1,32 @@
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
-    static Integer[] array = {};
+    private int min;
+    private int max;
+
 
     public Randoms(int min, int max) {
-        //...
-        Integer a = 0;
-        HashSet<Integer> r = new HashSet<Integer>();
-        while (r.size() < (max - min)) {
-            a = (int) ((Math.random() * (max - min + 1)) + min);
-            r.add((int) a);
-        }
-        array = r.toArray(r.toArray(new Integer[r.size()]));
-        System.out.println(Arrays.toString(array));
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator();
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
     }
 
 }
